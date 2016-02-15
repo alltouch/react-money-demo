3b1287d4ecf40b744c433f8c3bf366ef97b0daa7
Kirill Shavaran <alltouch@ukr.net>
2016-02-15 19:06:22 +0200

    restore old config

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,53 @@
 var path = require('path');
-var PATHS = {
-    app: path.join(__dirname, 'app'),
-    build: path.join(__dirname, 'build')
-};
+var node_modules = path.resolve(__dirname, 'node_modules');
+var pathToReact = path.resolve(node_modules, 'react/dist/react.min.js');
 
 module.exports = {
-    // Entry accepts a path or an object of entries. We'll be using the
-    // latter form given it's convenient with more complex configurations.
     entry: {
-        app: PATHS.app + '/main.js'
+        /*reload: [
+            'webpack/hot/dev-server',
+            'webpack-dev-server/client?http://localhost:8080'
+        ],*/
+        main: [
+            'webpack/hot/dev-server',
+            'webpack-dev-server/client?http://localhost:8080',
+            path.resolve(__dirname, 'app/main.js')
+        ]
+    },
+    resolve: {
+        alias: {
+            'react': pathToReact
+        }
     },
     output: {
-        path: PATHS.build,
-        filename: 'bundle.js'
+        path: path.resolve(__dirname, 'build'),
+        filename: "[name].js"
+    },
+    module: {
+        loaders: [
+            /*{
+                test: /\.css$/,
+                loader: "style!css"
+            },*/
+            {
+                test: /\.scss$/,
+                loader: 'style!css!sass'
+            },
+            {
+                test: /\.(png|jpg)$/,
+                loader: 'url?limit=15000'
+            },
+            {
+                test: /\.jsx?$/,
+                loader: 'babel',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ],
+        noParse: [
+            pathToReact
+        ]
     }
 };
