0b6865e1fef8179d0d2d540d568c043517e4ae80
Kirill Shavaran <alltouch@ukr.net>
2016-02-15 19:05:41 +0200

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+var path = require('path');
+var PATHS = {
+    app: path.join(__dirname, 'app'),
+    build: path.join(__dirname, 'build')
+};
+
+module.exports = {
+    // Entry accepts a path or an object of entries. We'll be using the
+    // latter form given it's convenient with more complex configurations.
+    entry: {
+        app: PATHS.app + '/main.js'
+    },
+    output: {
+        path: PATHS.build,
+        filename: 'bundle.js'
+    }
+};
